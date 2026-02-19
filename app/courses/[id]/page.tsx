b728fbfc429bc4b073e1535dd042ec6b71d2d6f0
Sowad <sowad.dev@gmail.com>
2026-02-19 06:37:57 +0600

    Fix view course page

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import { remove } from "@/app/lib/course";
 import Form from "next/form";
+import SubmitButton from "@/app/lib/SubmitButton";
 
 interface PageProps {
     params: Promise<{ id: string }>
@@ -35,15 +36,6 @@ export default async function Sections({ params }: PageProps) {
     if (!course) {
         notFound()
     }
-
-    const teachers = await prisma.teacher.findMany({
-        select: {
-            id: true,
-            name: true
-        }
-    })
-
-    const serializedCourse = JSON.parse(JSON.stringify(course));
 
     return (
         <div className="flex h-screen justify-center items-start pt-10">
@@ -115,12 +107,7 @@ export default async function Sections({ params }: PageProps) {
                         </a>
                         <Form action={remove}>
                             <input readOnly value={course.id} name="id" hidden />
-                            <button type="submit" className="flex items-cetner bg-red-800 text-white px-4 py-2 rounded hover:cursor-pointer">
-                                <span className="mr-2">Delete</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                </svg>
-                            </button>
+                            <SubmitButton text="Delete" />
                         </Form>
                     </div>
                 </div>
