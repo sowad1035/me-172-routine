f4d1a5c503f4f79c0e38d32e5242364b21e8e2c0
Sowad <sowad.dev@gmail.com>
2026-02-19 06:31:50 +0600

    Fix add course page

--- app/courses/add/page.tsx
+++ app/courses/add/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import CourseForm from "./CourseForm";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Sections() {
 
     const teachers = await prisma.teacher.findMany({
